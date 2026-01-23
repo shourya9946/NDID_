38ce63a76a22da1b8599189059a3aa7fb33d346f
shourya <shouryaprajapati9946@gmail.com>
2026-01-23 18:19:22 +0530

    Add main script to demonstrate vector comparison for image similarity

---
+++ d_main.py
@@ -0,0 +1,18 @@
+# import torch
+# import torch.nn as nn
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import numpy as np
+# import os
+# from torchvision.models import resnet18, ResNet18_Weights
+# from torchvision import transforms
+# from PIL import Image
+# import torch
+from c_vector_sim import __vector_cmp__
+
+
+img_folder_path = r"C:\Users\shour\OneDrive\Desktop\test_ukbench"
+img_1_name = "ukbench00000.jpg"
+img_2_name = "ukbench00001.jpg"
+alpha = 0.54
+print(__vector_cmp__(img_folder_path,img_1_name,img_folder_path,img_2_name,alpha))
